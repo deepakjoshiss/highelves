021cf915361a37b59330b4eb4315c40e632b3fbe
Deepak Joshi <deepakjoshi.th@gmail.com>
2022-07-30 22:08:04 +0530

    Better models with blender

---
+++ RunWithAssetBuilder.bat
@@ -0,0 +1,38 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGmW+0UiKRYUag2OOXj6Tq1EvqHHyuaCoVkPW+w2e8/6yKCdHNQb7wXIeoQplnVc2N8ECVtqdxysfUN0nWZH9laMJMLcvAmhXkCMpnw5D2Z8ymjZgiwHZNJ8m/8P1iG4sUT8kKte2HvwPg==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSjk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJH2F4EMMCj15aTiKNWyyA7kK7fvH3f+JlUQJXMMsa4rP+KGHLOQW+AvhbZNN
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@move art\compiledtextures art\textures
+@assetcachebuilder.exe
+@move art\textures art\compiledtextures
+start "" "runmod.lnk"
+@pause
